b00658c5be192de6e441799ae305db07259743c2
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-02-24 21:20:02 -0300

    handout 1 e 2

---
+++ Breakout/Assets/_Scripts/UI_MenuPrincipal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MenuPrincipal : MonoBehaviour
+{ 
+    GameManager gm;
+    private void OnEnable()
+    {
+        gm = GameManager.GetInstance();
+    }
+
+    public void Comecar()
+    {
+        gm.ChangeState(GameManager.GameState.GAME);
+    }
+}
