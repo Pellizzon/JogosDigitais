90d0763e62f0c106cc3141a9a192b45a5043c54b
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-03-03 15:04:23 -0300

    Shmup tutorial1

---
+++ Shmup/Assets/_Scripts/VolantBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class VolantBehaviour : SteerableBehaviour
+{
+    float angle = 0;
+
+    private void FixedUpdate()
+    {
+        angle += 0.1f;
+        if (angle > 2.0f * Mathf.PI) angle = 0.0f;
+        Thrust(0, Mathf.Cos(angle));
+    }
+}
---
+++ Shmup/README.md
@@ -0,0 +1,5 @@
+# Shmup
+
+## Jogos Digitais - 7o semestre - INSPER
+
+Asteroid: https://opengameart.org/content/a-layered-asteroid-rock
